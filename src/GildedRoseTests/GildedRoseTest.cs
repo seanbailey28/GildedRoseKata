625c5bfbaf9fbe7eea2a1b806c8f8b7666c40261
Sean Bailey <sean.bailey@orcha.co.uk>
2024-10-10 16:33:49 +0100

    Added tests to cover all base functionality described

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -1,18 +1,244 @@
 ﻿using Xunit;
-using System.Collections.Generic;
 using GildedRoseKata;
+using AutoFixture;
+using System.Linq;
+using FluentAssertions;
 
 namespace GildedRoseTests
 {
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
+        private readonly IFixture _fixture;
+
+        public GildedRoseTest()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public void Given_ItemNameIsFoo_When_QualityUpdateOccurs_Then_NameStaysTheSame()
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                .With(x => x.Name, "foo")
+                .CreateMany()
+                .ToList();
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.Name.Should().Be("foo"));
+        }
+
+        [Fact]
+        public void Given_SellInDaysIsOne_When_QualityUpdateOccurs_Then_DaysDecreasesByOne()
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                .With(x => x.SellIn, 1)
+                .CreateMany()
+                .ToList();
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.SellIn.Should().Be(0));
+        }
+
+        [Fact]
+        public void Given_SellByDateHasNotPassed_When_QualityUpdateOccurs_Then_QualityDegradesByOne()
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                .With(x => x.SellIn, 7)
+                .With(x => x.Quality, 5)
+                .CreateMany()
+                .ToList();
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.Quality.Should().Be(4));
+        }
+
+        [Theory]
+        [InlineData(0, 5)]
+        [InlineData(-1, 5)]
+        [InlineData(-100, 5)]
+        public void Given_SellByDateHasPassed_When_QualityUpdateOccurs_Then_QualityDegradesTwiceAsFast(int sellIn, int quality)
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                    .With(x => x.SellIn, sellIn)
+                    .With(x => x.Quality, quality)
+                    .CreateMany()
+                    .ToList();
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.Quality.Should().Be(3));
+        }
+
+        [Fact]
+        public void Given_QualityOfItemIsZero_When_QualityUpdateOccurs_Then_QualityIsNotDecreased()
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                .With(x => x.SellIn, 7)
+                .With(x => x.Quality, 0)
+                .CreateMany()
+                .ToList();
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.Quality.Should().Be(0));
+        }
+
+        [Fact]
+        public void Given_AgedBrieItem_When_QualityUpdateOccurs_Then_QualityIsIncreased()
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                .With(x => x.Name, "Aged Brie")
+                .With(x => x.Quality, 4)
+                .CreateMany()
+                .ToList();
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.Quality.Should().Be(5));
+        }
+
+        [Fact]
+        public void Given_AgedBrieItemQualityReachesFifty_When_QualityUpdateOccurs_Then_QualityIsNotIncreased()
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                .With(x => x.Name, "Aged Brie")
+                .With(x => x.Quality, 50)
+                .CreateMany()
+                .ToList();
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.Quality.Should().Be(50));
+        }
+
+        [Fact]
+        public void Given_LegendaryItem_When_QualityUpdateOccurs_Then_QualityDoesNotChange()
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                .With(x => x.Name, "Sulfuras, Hand of Ragnaros")
+                .With(x => x.Quality, 80)
+                .CreateMany()
+                .ToList();
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.Quality.Should().Be(80));
+        }
+
+        [Theory]
+        [InlineData(10, 5)]
+        [InlineData(8, 5)]
+        [InlineData(6, 5)]
+        public void Given_BackstagePassesHaveTenDaysOrLessToSell_When_QualityUpdateOccurs_Then_QualityIncreasesByTwo(int sellIn, int quality)
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                .With(x => x.Name, "Backstage passes to a TAFKAL80ETC concert")
+                .With(x => x.SellIn, sellIn)
+                .With(x => x.Quality, quality)
+                .CreateMany()
+                .ToList();
+
+            var expectedQuality = quality + 2;
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.Quality.Should().Be(expectedQuality));
+        }
+
+        [Theory]
+        [InlineData(5, 5)]
+        [InlineData(3, 5)]
+        [InlineData(1, 5)]
+        public void Given_BackstagePassesHaveFiveDaysOrLessToSell_When_QualityUpdateOccurs_Then_QualityIncreasesByThree(int sellIn, int quality)
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                .With(x => x.Name, "Backstage passes to a TAFKAL80ETC concert")
+                .With(x => x.SellIn, sellIn)
+                .With(x => x.Quality, quality)
+                .CreateMany()
+                .ToList();
+
+            var expectedQuality = quality + 3;
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.Quality.Should().Be(expectedQuality));
+        }
+
+        [Theory]
+        [InlineData(0, 100)]
+        [InlineData(-5, 30)]
+        [InlineData(-100, 5)]
+        public void Given_BackstagePassesHavePassedSellBy_When_QualityUpdateOccurs_Then_QualityDecreasesToZero(int sellIn, int quality)
+        {
+            // Arrange
+            var items = _fixture.Build<Item>()
+                .With(x => x.Name, "Backstage passes to a TAFKAL80ETC concert")
+                .With(x => x.SellIn, sellIn)
+                .With(x => x.Quality, quality)
+                .CreateMany()
+                .ToList();
+
+            GildedRose sut = new(items);
+
+            // Act
+            sut.UpdateQuality();
+
+            // Assert
+            items.Should().AllSatisfy(i => i.Quality.Should().Be(0));
         }
     }
 }
